a1867069522e8b375a3e3dada210c42c56f94cfb
Mkiw12 <149476629+Mkiw12@users.noreply.github.com>
2024-09-03 14:38:03 +0200

    updated 1b)

---
+++ proj1b.py
@@ -0,0 +1,33 @@
+import itertools
+
+# All permuations of 3 'U' and 4 'L'
+
+move = 'UUULLLL'
+
+unique_paths = set(itertools.permutations(move))
+
+valid_paths = []
+
+
+# Check each path for validity
+for path in unique_paths:
+    y = 3  # Start y-coordinate
+    valid = False  # Assume the path is invalid initially
+
+    for move in path:
+        if move == 'U':
+            y += 1
+        else:  # move == 'L'
+            y -= 1
+
+        # If the y-coordinate goes below or touches the x-axis
+        if y <= 0:
+            valid = True
+            break  # No need to check further for this path
+
+    if valid:
+        valid_paths.append(''.join(path))
+
+# Print all valid paths
+for valid_path in valid_paths:
+    print(valid_path)
