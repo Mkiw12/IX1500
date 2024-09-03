bfe1d547ee089136ccb4a031724158f3824c3fd0
jockipocky <jacob.j.emanuelsson@gmail.com>
2024-09-03 11:04:49 +0200

    uppgift c och d

---
+++ proj1d.py
@@ -0,0 +1,31 @@
+from itertools import permutations
+
+# Generate all permutations of 6 'U' and 7 'L'
+all_paths = set(permutations('UUUUUULLLLLLL'))
+
+valid_paths = []
+
+# Check each path
+for path in all_paths:
+    y = 6  # starting y-coordinate
+    is_valid = True
+    for move in path:
+        if move == 'U':
+            y += 1
+        else:  # move == 'L'
+            y -= 1
+        
+        if y <= 0:  # if at any point y <= 0, it touches or crosses the x-axis
+            is_valid = False
+            break
+    
+    if is_valid:
+        valid_paths.append(''.join(path))
+
+print(f"Number of valid paths: {len(valid_paths)}")
+print("Valid paths:")
+for path in valid_paths:
+    print(path)
+
+
+
