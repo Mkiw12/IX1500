f8f81a44d382f3164a67838be27888983751ea12
Mkiw12 <149476629+Mkiw12@users.noreply.github.com>
2024-09-03 12:35:35 +0200

    Finished a)

---
+++ Project 1/proj1a.py
@@ -0,0 +1,18 @@
+import itertools
+
+# Step 1: calculate number of U and L moves
+n = 7 # total number of moves
+k = 3
+U_move = k
+L_move = n - k
+
+# Step 2: create list of moves
+
+moves = ['L'] * L_move + ['U'] * U_move
+
+# Generate all unique permutaions of the moves
+unique_paths = set(itertools.permutations(moves))
+
+
+for path in unique_paths:
+    print(''.join(path))
